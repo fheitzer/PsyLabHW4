f224225765d6fad1f3c2d0305f0597789efe2e81
fheitzer <49273978+fheitzer@users.noreply.github.com>
2019-05-15 21:54:26 +0200

    s und d

--- 03_trials.js
+++ 03_trials.js
@@ -7,8 +7,8 @@ const key_press_trials = [
         picutre: 'mental_rotation_images/5_150_same.jpg'
         key1: 's',
         key2: 'd',
-        f: 'Same',
-        j: 'Different',
+        s: 'Same',
+        d: 'Different',
         expected: 'same',
         numberofpic: '5',
         rotation: '150'
@@ -18,8 +18,8 @@ const key_press_trials = [
         picutre: 'mental_rotation_images/6_50_same.jpg'
         key1: 's',
         key2: 'd',
-        f: 'Same',
-        j: 'Different',
+        s: 'Same',
+        d: 'Different',
         expected: 'Same',
         numberofpic: '6',
         rotation: '50'
@@ -30,8 +30,8 @@ const key_press_trials = [
         picutre: 'mental_rotation_images/10_150_different.jpg'
         key1: 's',
         key2: 'd',
-        f: 'Same',
-        j: 'Different',
+        s: 'Same',
+        d: 'Different',
         expected: 'Different',
         numberofpic: '10',
         rotation: '150'
@@ -41,8 +41,8 @@ const key_press_trials = [
         picutre: 'mental_rotation_images/1_50_same.jpg'
         key1: 's',
         key2: 'd',
-        f: 'Same',
-        j: 'Different',
+        s: 'Same',
+        d: 'Different',
         expected: 'Same',
         numberofpic: '1',
         rotation: '50'
@@ -52,8 +52,8 @@ const key_press_trials = [
         picutre: 'mental_rotation_images/3_50_different.jpg'
         key1: 's',
         key2: 'd',
-        f: 'Same',
-        j: 'Different',
+        s: 'Same',
+        d: 'Different',
         expected: 'Different',
         numberofpic: '3',
         rotation: '50'
